c11b386e7e1ddcf8ac4b1f335f0c2d5b6a577256
Carlos González <carlosgonal@gmail.com>
2019-10-24 19:02:48 +0200

    Api parse in python class

--- SpainOpenData/base.py
+++ SpainOpenData/base.py
@@ -1,14 +1,30 @@
 
 import json
+import os
+import pprint
+import shutil
+import time
+import urllib
+import urllib.request
 
+import pandas as pd
 import requests
 import urllib3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+pp = pprint.PrettyPrinter()
 
 
 class SpainOpenData():
-    ENDPOINT = "https://datos.gob.es/apidata/"
+    ENDPOINT_API = "https://datos.gob.es/apidata/{url}"
 
     def __init__(self, call_name=None, path=[], *args, **kwargs):
 
@@ -18,43 +34,43 @@ class SpainOpenData():
     def __getattr__(self, name):
         name = name.replace('_', '-')
         self._path.append(name)
-        print(self._path)
+        self._call_name = name
 
-        return SpainOpenData(
-            call_name=name,
-            path=self._path
-        )
+        return self
 
     def __call__(self, *args, **kwargs):
         """Callable for the object
         """
-
         parametes = [f'{key}={value}' for key, value in kwargs.items()]
         url = "/".join(self._path) + '?' + "&".join(parametes)
-        url = self.ENDPOINT + url
+        url = self.ENDPOINT_API.format(url=url)
 
-        print(
-            f"Wrapping: {self._path} Args: '{args}'. Kwargs: '{kwargs}' \n {url}"
-        )
+        response = requests.request("GET", url, verify=False)
+
+        if not response.ok:
+            attribute = self._path[-1]
+            message = f"AttributeError: '{self.__class__.__name__}' object has no attribute '{attribute}'"
+            raise AttributeError(message)
 
         self._path = []
-        print(self, self._path)
-        try:
-            response = requests.request("GET", url, verify=False)
-            return response.json()
+        return response.json()['result']['items']
 
-        except Exception as error:
-            print("error")
-            print(error)
-            return None
 
 
 def main():
+    pp = pprint.PrettyPrinter()
     spapi = SpainOpenData()
 
-    result = spapi.catalog.dataset(_sort='title', _pageSize=10, _page=0)
-    print(spapi._path)
-    result = spapi.catalog.dataset.l01281230_calidad_del_aire()
+    result = spapi.catalog.dataset(_sort='title', _pageSize=1, _page=0)
+    pp.pprint(result)
+    result = spapi.catalog.dataset.l01281230_calidad_del_aire(_sort='title', _pageSize=10, _page=0)
+    pp.pprint(result)
+    # dataf = spapi.get_dataset(
+    #     dataset_id="u03400001-estudiantes-matriculados-en-estudios-oficiales")
+
+    # print(dataf)
+    pp = pprint.PrettyPrinter()
+    # pp.pprint(result)
 
 
 if __name__ == "__main__":
